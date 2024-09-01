dcdc6280afc95e161b9b734c0dfd48291ebc18d2
Audionut <audionut11@gmail.com>
2024-09-02 09:26:14 +1000

    Lint cleaning

--- src/trackers/SHRI.py
+++ src/trackers/SHRI.py
@@ -3,7 +3,6 @@
 import asyncio
 import requests
 from str2bool import str2bool
-import os
 import platform
 
 from src.trackers.COMMON import COMMON
@@ -20,7 +19,7 @@ class SHRI():
     """
 
     ###############################################################
-    ########                    EDIT ME                    ########
+    ########                    EDIT ME                    ########  # noqa #E266
     ###############################################################
 
     # ALSO EDIT CLASS NAME ABOVE
@@ -31,46 +30,46 @@ class SHRI():
         self.source_flag = 'Shareisland'
         self.upload_url = 'https://shareisland.org/api/torrents/upload'
         self.search_url = 'https://shareisland.org/api/torrents/filter'
-        self.signature = f"\n[center][url=https://shareisland.org]Created by SHRI Upload Assistant[/url][/center]"
+        self.signature = "\n[center][url=https://shareisland.org]Created by SHRI Upload Assistant[/url][/center]"
         self.banned_groups = [""]
         pass
-    
+
     async def get_cat_id(self, category_name):
         category_id = {
-            'MOVIE': '1', 
-            'TV': '2', 
-            }.get(category_name, '0')
+            'MOVIE': '1',
+            'TV': '2',
+        }.get(category_name, '0')
         return category_id
 
     async def get_type_id(self, type):
         type_id = {
-            'DISC': '26', 
+            'DISC': '26',
             'REMUX': '7',
-            'WEBDL': '27', 
-            'WEBRIP': '27', 
+            'WEBDL': '27',
+            'WEBRIP': '27',
             'HDTV': '6',
             'ENCODE': '15'
-            }.get(type, '0')
+        }.get(type, '0')
         return type_id
 
     async def get_res_id(self, resolution):
         resolution_id = {
-            '8640p':'10', 
-            '4320p': '1', 
-            '2160p': '2', 
-            '1440p' : '3',
+            '8640p': '10',
+            '4320p': '1',
+            '2160p': '2',
+            '1440p': '3',
             '1080p': '3',
-            '1080i':'4', 
-            '720p': '5',  
-            '576p': '6', 
+            '1080i': '4',
+            '720p': '5',
+            '576p': '6',
             '576i': '7',
-            '480p': '8', 
+            '480p': '8',
             '480i': '9'
-            }.get(resolution, '10')
+        }.get(resolution, '10')
         return resolution_id
 
     ###############################################################
-    ######   STOP HERE UNLESS EXTRA MODIFICATION IS NEEDED   ######
+    ######   STOP HERE UNLESS EXTRA MODIFICATION IS NEEDED   ######  # noqa #E266
     ###############################################################
 
     async def upload(self, meta):
@@ -82,12 +81,12 @@ class SHRI():
         await common.unit3d_edit_desc(meta, self.tracker, self.signature)
         region_id = await common.unit3d_region_ids(meta.get('region'))
         distributor_id = await common.unit3d_distributor_ids(meta.get('distributor'))
-        if meta['anon'] == 0 and bool(str2bool(str(self.config['TRACKERS'][self.tracker].get('anon', "False")))) == False:
+        if meta['anon'] == 0 and bool(str2bool(str(self.config['TRACKERS'][self.tracker].get('anon', "False")))) is False:
             anon = 0
         else:
             anon = 1
 
-        if meta['bdinfo'] != None:
+        if meta['bdinfo'] is not None:
             mi_dump = None
             bd_dump = open(f"{meta['base_dir']}/tmp/{meta['uuid']}/BD_SUMMARY_00.txt", 'r', encoding='utf-8').read()
         else:
@@ -97,34 +96,34 @@ class SHRI():
         open_torrent = open(f"{meta['base_dir']}/tmp/{meta['uuid']}/[{self.tracker}]{meta['clean_name']}.torrent", 'rb')
         files = {'torrent': open_torrent}
         data = {
-            'name' : meta['name'],
-            'description' : desc,
-            'mediainfo' : mi_dump,
-            'bdinfo' : bd_dump, 
-            'category_id' : cat_id,
-            'type_id' : type_id,
-            'resolution_id' : resolution_id,
-            'tmdb' : meta['tmdb'],
-            'imdb' : meta['imdb_id'].replace('tt', ''),
-            'tvdb' : meta['tvdb_id'],
-            'mal' : meta['mal_id'],
-            'igdb' : 0,
-            'anonymous' : anon,
-            'stream' : meta['stream'],
-            'sd' : meta['sd'],
-            'keywords' : meta['keywords'],
-            'personal_release' : int(meta.get('personalrelease', False)),
-            'internal' : 0,
-            'featured' : 0,
-            'free' : 0,
-            'doubleup' : 0,
-            'sticky' : 0,
+            'name': meta['name'],
+            'description': desc,
+            'mediainfo': mi_dump,
+            'bdinfo': bd_dump,
+            'category_id': cat_id,
+            'type_id': type_id,
+            'resolution_id': resolution_id,
+            'tmdb': meta['tmdb'],
+            'imdb': meta['imdb_id'].replace('tt', ''),
+            'tvdb': meta['tvdb_id'],
+            'mal': meta['mal_id'],
+            'igdb': 0,
+            'anonymous': anon,
+            'stream': meta['stream'],
+            'sd': meta['sd'],
+            'keywords': meta['keywords'],
+            'personal_release': int(meta.get('personalrelease', False)),
+            'internal': 0,
+            'featured': 0,
+            'free': 0,
+            'doubleup': 0,
+            'sticky': 0,
         }
         # Internal
-        if self.config['TRACKERS'][self.tracker].get('internal', False) == True:
+        if self.config['TRACKERS'][self.tracker].get('internal', False) is True:
             if meta['tag'] != "" and (meta['tag'][1:] in self.config['TRACKERS'][self.tracker].get('internal_groups', [])):
                 data['internal'] = 1
-                
+
         if region_id != 0:
             data['region_id'] = region_id
         if distributor_id != 0:
@@ -136,18 +135,18 @@ class SHRI():
             'User-Agent': f'Upload Assistant/2.1 ({platform.system()} {platform.release()})'
         }
         params = {
-            'api_token' : self.config['TRACKERS'][self.tracker]['api_key'].strip()
+            'api_token': self.config['TRACKERS'][self.tracker]['api_key'].strip()
         }
-        
-        if meta['debug'] == False:
+
+        if meta['debug'] is False:
             response = requests.post(url=self.upload_url, files=files, data=data, headers=headers, params=params)
             try:
                 console.print(response.json())
-            except:
+            except Exception:
                 console.print("It may have uploaded, go check")
-                return 
+                return
         else:
-            console.print(f"[cyan]Request Data:")
+            console.print("[cyan]Request Data:")
             console.print(data)
         open_torrent.close()
 
@@ -155,12 +154,12 @@ class SHRI():
         dupes = []
         console.print("[yellow]Searching for existing torrents on site...")
         params = {
-            'api_token' : self.config['TRACKERS'][self.tracker]['api_key'].strip(),
-            'tmdbId' : meta['tmdb'],
-            'categories[]' : await self.get_cat_id(meta['category']),
-            'types[]' : await self.get_type_id(meta['type']),
-            'resolutions[]' : await self.get_res_id(meta['resolution']),
-            'name' : ""
+            'api_token': self.config['TRACKERS'][self.tracker]['api_key'].strip(),
+            'tmdbId': meta['tmdb'],
+            'categories[]': await self.get_cat_id(meta['category']),
+            'types[]': await self.get_type_id(meta['type']),
+            'resolutions[]': await self.get_res_id(meta['resolution']),
+            'name': ""
         }
         if meta.get('edition', "") != "":
             params['name'] = params['name'] + f" {meta['edition']}"
@@ -172,7 +171,7 @@ class SHRI():
                 # difference = SequenceMatcher(None, meta['clean_name'], result).ratio()
                 # if difference >= 0.05:
                 dupes.append(result)
-        except:
+        except Exception:
             console.print('[bold red]Unable to search for existing torrents on site. Either the site is down or your API key is incorrect')
             await asyncio.sleep(5)
 
