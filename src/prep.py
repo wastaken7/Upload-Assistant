dccd28c970c22b687d253b72b91026ca018230ef
Audionut <audionut11@gmail.com>
2024-09-12 16:49:19 +1000

    Export cleanpath mediainfo with DVD, for MTV

--- src/prep.py
+++ src/prep.py
@@ -682,6 +682,9 @@ class Prep():
             export = open(f"{meta['base_dir']}/tmp/{meta['uuid']}/MEDIAINFO.txt", 'w', newline="", encoding='utf-8')
             export.write(discs[0]['ifo_mi'])
             export.close()
+            export_clean = open(f"{meta['base_dir']}/tmp/{meta['uuid']}/MEDIAINFO_CLEANPATH.txt", 'w', newline="", encoding='utf-8')
+            export_clean.write(discs[0]['ifo_mi'])
+            export_clean.close()
         elif is_disc == "HDDVD":
             discs = await parse.get_hddvd_info(discs)
             export = open(f"{meta['base_dir']}/tmp/{meta['uuid']}/MEDIAINFO.txt", 'w', newline="", encoding='utf-8')
