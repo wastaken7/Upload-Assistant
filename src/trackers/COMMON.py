ff0badb5d11b8df4624a11e685675dd8b2227a12
Audionut <audionut11@gmail.com>
2024-10-30 00:33:13 +1000

    DVD and other minor fixes

--- src/trackers/COMMON.py
+++ src/trackers/COMMON.py
@@ -62,10 +62,18 @@ class COMMON():
             descfile.write(desc)
             # Handle single disc case
             if len(discs) == 1:
+                each = discs[0]
+                if each['type'] == "DVD":
+                    descfile.write("[center]")
+                    descfile.write(f"[spoiler={os.path.basename(each['vob'])}][code]{each['vob_mi']}[/code][/spoiler]\n\n")
+                    descfile.write("[/center]")
                 images = meta['image_list']
+                descfile.write("[center]")
                 for img_index in range(len(images[:int(meta['screens'])])):
+                    web_url = images[img_index]['web_url']
                     raw_url = images[img_index]['raw_url']
-                    descfile.write(f"[img={self.config['DEFAULT'].get('thumbnail_size', '350')}] {raw_url}[/img]\n")
+                    descfile.write(f"[url={web_url}][img={self.config['DEFAULT'].get('thumbnail_size', '350')}]{raw_url}[/img][/url] ")
+                descfile.write("[/center]")
 
             # Handle multiple discs case
             elif len(discs) > 1:
@@ -83,13 +91,17 @@ class COMMON():
                             descfile.write(f"{each.get('name', 'BDINFO')}\n\n")
                         elif each['type'] == "DVD":
                             descfile.write(f"{each['name']}:\n")
+                            descfile.write(f"[spoiler={os.path.basename(each['vob'])}][code]{each['vob_mi']}[/code][/spoiler]")
+                            descfile.write(f"[spoiler={os.path.basename(each['ifo'])}][code]{each['ifo_mi']}[/code][/spoiler]\n\n")
                         # For the first disc, use images from `meta['image_list']`
                         if meta['debug']:
                             console.print("[yellow]Using original uploaded images for first disc")
                         images = meta['image_list']
                         for img_index in range(min(multi_screens, len(images))):
+                            web_url = images[img_index]['web_url']
                             raw_url = images[img_index]['raw_url']
-                            descfile.write(f"[img=300]{raw_url}[/img] ")
+                            image_str = f"[url={web_url}][img=300]{raw_url}[/img][/url] "
+                            descfile.write(image_str)
                         descfile.write("[/center]\n\n")
                     else:
                         # Check if screenshots exist for the current disc key
@@ -107,8 +119,10 @@ class COMMON():
                             # Use existing URLs from meta to write to descfile
                             descfile.write("[center]")
                             for img in meta[new_images_key]:
+                                web_url = img['web_url']
                                 raw_url = img['raw_url']
-                                descfile.write(f"[img=300]{raw_url}[/img] ")
+                                image_str = f"[url={web_url}][img=300]{raw_url}[/img][/url] "
+                                descfile.write(image_str)
                             descfile.write("[/center]\n\n")
                         else:
                             # Increment retry_count for tracking but use unique disc keys for each disc
@@ -123,27 +137,30 @@ class COMMON():
                                 descfile.write(f"[spoiler={os.path.basename(each['ifo'])}][code]{each['ifo_mi']}[/code][/spoiler]\n\n")
                             descfile.write("[/center]\n\n")
                             # Check if new screenshots already exist before running prep.screenshots
-                            new_screens = glob.glob1(f"{meta['base_dir']}/tmp/{meta['uuid']}", f"FILE_{i}-*.png")
+                            if each['type'] == "BDMV":
+                                new_screens = glob.glob1(f"{meta['base_dir']}/tmp/{meta['uuid']}", f"FILE_{i}-*.png")
+                            elif each['type'] == "DVD":
+                                new_screens = glob.glob1(f"{meta['base_dir']}/tmp/{meta['uuid']}", f"{meta['discs'][i]['name']}-*.png")
                             if not new_screens:
                                 if meta['debug']:
                                     console.print(f"[yellow]No new screens for {new_images_key}; creating new screenshots")
                                 # Run prep.screenshots if no screenshots are present
-                                use_vs = meta.get('vapoursynth', False)
-                                s = multiprocessing.Process(target=prep.disc_screenshots, args=(f"FILE_{i}", each['bdinfo'], meta['uuid'], meta['base_dir'], use_vs, [], meta.get('ffdebug', False), multi_screens))
-                                s.start()
-                                while s.is_alive():
-                                    await asyncio.sleep(1)
-
-                                # Re-check for new screenshots after screenshots process
-                                new_screens = glob.glob1(f"{meta['base_dir']}/tmp/{meta['uuid']}", f"FILE_{i}-*.png")
+                                if each['type'] == "BDMV":
+                                    use_vs = meta.get('vapoursynth', False)
+                                    s = multiprocessing.Process(target=prep.disc_screenshots, args=(f"FILE_{i}", each['bdinfo'], meta['uuid'], meta['base_dir'], use_vs, [], meta.get('ffdebug', False), multi_screens))
+                                    s.start()
+                                    while s.is_alive():
+                                        await asyncio.sleep(1)
+                                    new_screens = glob.glob1(f"{meta['base_dir']}/tmp/{meta['uuid']}", f"FILE_{i}-*.png")
+                                elif each['type'] == "DVD":
+                                    s = multiprocessing.Process(target=prep.dvd_screenshots, args=(meta, i, multi_screens))
+                                    s.start()
+                                    while s.is_alive() is True:
+                                        await asyncio.sleep(1)
+                                    new_screens = glob.glob1(f"{meta['base_dir']}/tmp/{meta['uuid']}", f"{meta['discs'][i]['name']}-*.png")
 
                             if new_screens:
-                                uploaded_images, _ = prep.upload_screens(
-                                    meta,
-                                    multi_screens, 1, 0, 2,
-                                    new_screens,
-                                    {new_images_key: meta[new_images_key]}
-                                )
+                                uploaded_images, _ = prep.upload_screens(meta, multi_screens, 1, 0, 2, new_screens, {new_images_key: meta[new_images_key]})
 
                                 # Append each uploaded image's data to `meta[new_images_key]`
                                 for img in uploaded_images:
@@ -156,14 +173,16 @@ class COMMON():
                                 # Write new URLs to descfile
                                 descfile.write("[center]")
                                 for img in uploaded_images:
+                                    web_url = img['web_url']
                                     raw_url = img['raw_url']
-                                    descfile.write(f"[img=300]{raw_url}[/img] ")
+                                    image_str = f"[url={web_url}][img=300]{raw_url}[/img][/url] "
+                                    descfile.write(image_str)
                                 descfile.write("[/center]\n\n")
 
-                                # Save the updated meta to `meta.json` after upload
-                                meta_filename = f"{meta['base_dir']}/tmp/{meta['uuid']}/meta.json"
-                                with open(meta_filename, 'w') as f:
-                                    json.dump(meta, f, indent=4)
+                            # Save the updated meta to `meta.json` after upload
+                            meta_filename = f"{meta['base_dir']}/tmp/{meta['uuid']}/meta.json"
+                            with open(meta_filename, 'w') as f:
+                                json.dump(meta, f, indent=4)
 
             # Handle single file case
             if len(filelist) == 1:
@@ -172,7 +191,7 @@ class COMMON():
                 for img_index in range(len(images[:int(meta['screens'])])):
                     web_url = images[img_index]['web_url']
                     raw_url = images[img_index]['raw_url']
-                    descfile.write(f"[url={web_url}][img={self.config['DEFAULT'].get('thumbnail_size', '350')}] {raw_url}[/img][/url] ")
+                    descfile.write(f"[url={web_url}][img={self.config['DEFAULT'].get('thumbnail_size', '350')}]{raw_url}[/img][/url] ")
                 descfile.write("[/center]")
 
             # Handle multiple files case
