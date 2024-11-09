910e0bdca8107ff115b546fb7c3e5d5fd5a63062
Audionut <audionut11@gmail.com>
2024-11-09 15:55:09 +1000

    Fix argument overrides

--- upload.py
+++ upload.py
@@ -157,16 +157,29 @@ async def do_the_thing(base_dir):
         try:
             with open(f"{base_dir}/tmp/{os.path.basename(path)}/meta.json") as f:
                 saved_meta = json.load(f)
+
+                # Define the list of keys that can be overwritten
+                overwrite_list = [
+                    'trackers', 'dupe', 'debug', 'anon', 'category', 'type', 'screens', 'nohash', 'manual_edition', 'imdb', 'tmdb_manual', 'mal', 'manual',
+                    'hdb', 'ptp', 'blu', 'no_season', 'no_aka', 'no_year', 'no_dub', 'no_tag', 'no_seed', 'client', 'desclink', 'descfile', 'desc', 'draft',
+                    'modq', 'region', 'freeleech', 'personalrelease', 'unattended', 'manual_season', 'manual_episode', 'torrent_creation', 'qbit_tag', 'qbit_cat',
+                    'skip_imghost_upload', 'imghost', 'manual_source', 'webdv', 'hardcoded-subs', 'dual_audio'
+                ]
+
+                sanitized_saved_meta = {}
                 for key, value in saved_meta.items():
-                    overwrite_list = [
-                        'trackers', 'dupe', 'debug', 'anon', 'category', 'type', 'screens', 'nohash', 'manual_edition', 'imdb', 'tmdb_manual', 'mal', 'manual',
-                        'hdb', 'ptp', 'blu', 'no_season', 'no_aka', 'no_year', 'no_dub', 'no_tag', 'no_seed', 'client', 'desclink', 'descfile', 'desc', 'draft', 'modq', 'region', 'freeleech',
-                        'personalrelease', 'unattended', 'season', 'episode', 'torrent_creation', 'qbit_tag', 'qbit_cat', 'skip_imghost_upload', 'imghost', 'manual_source', 'webdv', 'hardcoded-subs', 'dual_audio'
-                    ]
-                    if meta.get(key, None) != value and key in overwrite_list:
-                        saved_meta[key] = meta[key]
-                meta = saved_meta
-                f.close()
+                    clean_key = key.strip().strip("'").strip('"')
+
+                    if clean_key in overwrite_list:
+                        if clean_key in meta and meta.get(clean_key) is not None:
+                            sanitized_saved_meta[clean_key] = meta[clean_key]
+                            if meta['debug']:
+                                console.print(f"Overriding {clean_key} with meta value:", meta[clean_key])
+                        else:
+                            sanitized_saved_meta[clean_key] = value
+                    else:
+                        sanitized_saved_meta[clean_key] = value
+                meta.update(sanitized_saved_meta)
         except FileNotFoundError:
             pass
         console.print(f"[green]Gathering info for {os.path.basename(path)}")
