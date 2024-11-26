7b95c0c918d2f752542f6ed981ea83a3b560ef6b
Audionut <audionut11@gmail.com>
2024-11-27 01:09:38 +1000

    Fix header again

--- data/example-config.py
+++ data/example-config.py
@@ -57,7 +57,7 @@ config = {
         "processLimit": "10",
 
         # Providing the option to add a header, in bbcode, above the screenshot section where supported
-        # "screenshot_header": "[centers] SCREENSHOTS [/center]",
+        # "screenshot_header": "[center] SCREENSHOTS [/center]",
 
         # Enable lossless PNG Compression (True/False)
         "optimize_images": True,
