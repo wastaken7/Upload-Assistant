551b2d2d206f919dab6dbadebae8b9484fcc9fcf
Audionut <audionut11@gmail.com>
2024-12-29 11:51:11 +1000

    Fix PTP/THR when imdb id is 0

--- src/trackerstatus.py
+++ src/trackerstatus.py
@@ -37,8 +37,11 @@ async def process_all_trackers(meta):
             tracker_class = tracker_class_map[tracker_name](config=config)
             if tracker_name in {"THR", "PTP"}:
                 if local_meta.get('imdb_id', '0') == '0':
-                    imdb_id = "0000000" if local_meta['unattended'] else cli_ui.ask_string("Unable to find IMDB id, please enter e.g.(tt1234567)")
-                    meta['imdb_id'] = imdb_id.replace('tt', '').zfill(7)
+                    imdb_id = "0" if local_meta['unattended'] else cli_ui.ask_string("Unable to find IMDB id, please enter e.g.(tt1234567)")
+                    if imdb_id == "":
+                        meta['imdb_id'] = "0"
+                    else:
+                        meta['imdb_id'] = imdb_id.replace('tt', '').zfill(7)
 
             if tracker_name == "PTP":
                 console.print("[yellow]Searching for Group ID")
