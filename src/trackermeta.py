a68ce744db04033fbd4ce8fae93b7333ab0996f1
Audionut <audionut11@gmail.com>
2025-02-11 00:41:04 +1000

    HDB correct imdb meta set

--- src/trackermeta.py
+++ src/trackermeta.py
@@ -261,7 +261,7 @@ async def update_metadata_from_tracker(tracker_name, tracker_instance, meta, sea
             console.print("[cyan]Using specified PTP ID to get IMDb ID[/cyan]")
             imdb_id, _, meta['ext_torrenthash'] = await tracker_instance.get_imdb_from_torrent_id(ptp_torrent_id)
             if imdb_id:
-                meta['imdb'] = str(imdb_id).zfill(7)
+                meta['imdb_id'] = str(imdb_id).zfill(7)
                 console.print(f"[green]IMDb ID found: tt{meta['imdb']}[/green]")
                 found_match = True
                 meta['skipit'] = True
@@ -290,7 +290,7 @@ async def update_metadata_from_tracker(tracker_name, tracker_instance, meta, sea
             imdb, tvdb_id, hdb_name, meta['ext_torrenthash'] = await tracker_instance.get_info_from_torrent_id(meta[tracker_key])
 
             if imdb or tvdb_id:
-                meta['imdb'] = str(imdb).zfill(7) if imdb else None
+                meta['imdb_id'] = str(imdb).zfill(7) if imdb else None
                 meta['tvdb_id'] = str(tvdb_id) if tvdb_id else meta.get('tvdb_id')
                 meta['hdb_name'] = hdb_name
                 found_match = True
@@ -304,7 +304,7 @@ async def update_metadata_from_tracker(tracker_name, tracker_instance, meta, sea
             # Use search_filename function if ID is not found in meta
             imdb, tvdb_id, hdb_name, meta['ext_torrenthash'], tracker_id = await tracker_instance.search_filename(search_term, search_file_folder, meta)
 
-            meta['imdb'] = str(imdb).zfill(7) if imdb else None
+            meta['imdb_id'] = str(imdb).zfill(7) if imdb else None
             meta['tvdb_id'] = str(tvdb_id) if tvdb_id else meta.get('tvdb_id')
             meta['hdb_name'] = hdb_name
             if tracker_id:
@@ -320,7 +320,7 @@ async def update_metadata_from_tracker(tracker_name, tracker_instance, meta, sea
                         console.print(f"[yellow]{tracker_name} data discarded.[/yellow]")
                         meta[tracker_key] = None
                         meta['tvdb_id'] = None
-                        meta['imdb'] = None
+                        meta['imdb_id'] = None
                         meta['hdb_name'] = None
                         found_match = False
                 else:
