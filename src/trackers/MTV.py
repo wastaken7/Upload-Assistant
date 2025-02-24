9c0e94ae116928f0ab417e45529626562f19a6a5
Audionut <audionut11@gmail.com>
2025-02-24 18:13:40 +1000

    MTV - dual audio shenanigans

--- src/trackers/MTV.py
+++ src/trackers/MTV.py
@@ -228,7 +228,6 @@ class MTV():
         else:
             mtv_name = meta['name']
             prefix_removed = False
-            prefix_index = -1  # This will record the location where the token was found.
             tokens = ("Dual-Audio ", "Dubbed ")
 
             for token in tokens:
@@ -239,20 +238,22 @@ class MTV():
                     # Remove only the first occurrence of the token.
                     mtv_name = mtv_name[:index] + mtv_name[index + len(token):]
                     break
-            console.print(mtv_name)
             audio_str = meta['audio']
             if prefix_removed:
                 # Remove the token from the audio string too.
                 for token in tokens:
                     audio_str = audio_str.replace(token, "")
+
+            if prefix_removed and prefix_index != -1:
+                mtv_name = f"{mtv_name[:prefix_index]}DUAL {mtv_name[prefix_index:].lstrip()}"
+
             if meta.get('type') in ('WEBDL', 'WEBRIP', 'ENCODE') and "DD" in audio_str:
                 mtv_name = mtv_name.replace(audio_str, audio_str.replace(' ', '', 1))
             if meta['category'] == "TV" and meta.get('tv_pack', 0) == 0 and meta.get('episode_title_storage', '').strip() != '' and meta['episode'].strip() != '':
                 mtv_name = mtv_name.replace(meta['episode'], f"{meta['episode']} {meta['episode_title_storage']}")
             if 'DD+' in meta.get('audio', '') and 'DDP' in meta['uuid']:
                 mtv_name = mtv_name.replace('DD+', 'DDP')
-            if prefix_removed and prefix_index != -1:
-                mtv_name = mtv_name[:prefix_index] + "DUAL " + mtv_name[prefix_index:]
+
         if meta['source'].lower().replace('-', '') in mtv_name.replace('-', '').lower():
             if not meta['isdir']:
                 # Check if there is a valid file extension, otherwise, skip the split
