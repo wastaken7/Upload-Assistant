6546850bf2d013c6d9de7f33b6bff08c4251590d
Audionut <audionut11@gmail.com>
2025-02-26 18:30:26 +1000

    Aither - skip language if commentary

--- src/trackers/AITHER.py
+++ src/trackers/AITHER.py
@@ -152,7 +152,10 @@ class AITHER():
                     audio_section = re.findall(r'Audio[\s\S]+?Language\s+:\s+(\w+)', media_info_text)
                     for i, language in enumerate(audio_section):
                         language = language.lower().strip()
-                        if language.lower().startswith('en'):  # Check if it's English
+                        if language.lower().startswith('en'):
+                            title_match = re.findall(r'Audio[\s\S]+?Title\s+:\s+(.+)', media_info_text)
+                            if title_match and "commentary" in title_match[i].lower():
+                                continue
                             return True
                 return False
 
