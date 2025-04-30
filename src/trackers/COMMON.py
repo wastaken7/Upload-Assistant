cfd1636b4e856b4e68fd0250846958faa4efafac
GizmoBal <truc.muche.atlantis@gmail.com>
2025-04-30 14:31:58 +0200

    Further bbcode conversions

--- src/trackers/COMMON.py
+++ src/trackers/COMMON.py
@@ -172,7 +172,7 @@ class COMMON():
             desc = bbcode.convert_pre_to_code(desc)
             desc = bbcode.convert_hide_to_spoiler(desc)
             desc = desc.replace("[user]", "").replace("[/user]", "")
-            desc = desc.replace("[list]", "").replace("[/list]", "")
+            desc = desc.replace("[hr]", "").replace("[/hr]", "")
             desc = desc.replace("[ul]", "").replace("[/ul]", "")
             desc = desc.replace("[ol]", "").replace("[/ol]", "")
             if comparison is False:
