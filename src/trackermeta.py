d689e2c811f1a4c4a974fcb8e5ae459cd121f0ef
Audionut <audionut11@gmail.com>
2025-05-31 15:41:34 +1000

    catch MOVIES is some unit3d response

--- src/trackermeta.py
+++ src/trackermeta.py
@@ -234,6 +234,7 @@ async def update_meta_with_unit3d_data(meta, tracker_data, tracker_name, only_id
                 description.write((desc or "") + "\n")
     if category and ('MOVIE' in category.upper() or 'TV' in category.upper()):
         meta['category'] = category.upper()
+        meta['category'] = meta['category'].replace('MOVIES', 'MOVIE')
         if meta['debug']:
             console.print("set Category:", meta['category'])
 
