0a286786b69b19ac5fa010b79e4aafc846671324
Audionut <audionut11@gmail.com>
2025-07-09 16:02:56 +1000

    Revert "fix common id searching"
    This reverts commit 56f4d61a4f6b51d9d16fcd119ccac14aea1ce0c4.

--- src/trackers/COMMON.py
+++ src/trackers/COMMON.py
@@ -940,8 +940,7 @@ class COMMON():
                 console.print(f"[green]Searching {tracker} by file name: [bold yellow]{file_name}[/bold yellow]")
             url = search_url
         elif id:
-            params['id'] = id
-            url = f"{search_url}"
+            url = f"{torrent_url}{id}"
             if meta.get('debug'):
                 console.print(f"[green]Searching {tracker} by ID: [bold yellow]{id}[/bold yellow] via {url}")
         else:
