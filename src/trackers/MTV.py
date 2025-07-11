1320b8e033222221d2a3fe51bd289edc83c58746
Audionut <audionut11@gmail.com>
2025-07-11 17:20:41 +1000

    MTV only spoiler with text

--- src/trackers/MTV.py
+++ src/trackers/MTV.py
@@ -219,8 +219,9 @@ class MTV():
                     img_url = image['img_url']
                     desc.write(f"[url={raw_url}][img=250]{img_url}[/img][/url]")
 
-            base = re.sub(r'\[/?quote\]', '', base, flags=re.IGNORECASE)
-            desc.write(f"\n\n[spoiler=Notes]{base}[/spoiler]")
+            base = re.sub(r'\[/?quote\]', '', base, flags=re.IGNORECASE).strip()
+            if base != "":
+                desc.write(f"\n\n[spoiler=Notes]{base}[/spoiler]")
             desc.close()
         return
 
