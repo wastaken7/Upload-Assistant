7b6292ec72018393661787a884885804bcb94de5
Hielito <36553765+Hielito2@users.noreply.github.com>
2025-07-21 22:19:45 +0000

    LT.py SUBS parser failed Spanish (AR) (#626)
    * LT - Spanish audio
    
    * LT - Spanish audio
    
    * LT - Spanish audio

--- src/trackers/LT.py
+++ src/trackers/LT.py
@@ -93,9 +93,9 @@ class LT():
                 audio for audio in meta['mediainfo']['media']['track'][2:]
                 if audio.get('@type') == 'Audio'
                 and isinstance(audio.get('Language'), str)
-                and audio.get('Language') in {'es-419', 'es', 'es-mx', 'es-ar', 'es-cl', 'es-ve', 'es-bo', 'es-co',
-                                              'es-cr', 'es-do', 'es-ec', 'es-sv', 'es-gt', 'es-hn', 'es-ni', 'es-pa',
-                                              'es-py', 'es-pe', 'es-pr', 'es-uy'}
+                and audio.get('Language').lower() in {'es-419', 'es', 'es-mx', 'es-ar', 'es-cl', 'es-ve', 'es-bo', 'es-co',
+                                                      'es-cr', 'es-do', 'es-ec', 'es-sv', 'es-gt', 'es-hn', 'es-ni', 'es-pa',
+                                                      'es-py', 'es-pe', 'es-pr', 'es-uy'}
                 and "commentary" not in str(audio.get('Title', '')).lower()
                 ]
             if len(audios) > 0:  # If there is at least 1 audio spanish
