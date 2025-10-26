624a825318f960c47f9f14638c35e2097b7f4055
Audionut <audionut11@gmail.com>
2025-10-26 18:35:07 +1000

    HUNO: screens per row fix
    Stay under 1100 pixels

--- src/trackers/COMMON.py
+++ src/trackers/COMMON.py
@@ -172,6 +172,11 @@ class COMMON():
         try:
             # If screensPerRow is set, use that to determine how many screenshots should be on each row. Otherwise, use 2 as default
             screensPerRow = int(self.config['DEFAULT'].get('screens_per_row', 2))
+            if tracker == "HUNO":
+                width = int(self.config['DEFAULT'].get('thumbnail_size', '350'))
+                # Adjust screensPerRow to keep total width below 1100
+                while screensPerRow * width > 1100 and screensPerRow > 1:
+                    screensPerRow -= 1
         except Exception:
             screensPerRow = 2
         if tracker == "HUNO" and signature != "":
