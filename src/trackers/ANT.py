d3c552886ce7188954eff07115431ef23fd606a6
Audionut <audionut11@gmail.com>
2025-11-10 20:42:39 +1000

    ANT: prohibit bloated

--- src/trackers/ANT.py
+++ src/trackers/ANT.py
@@ -241,6 +241,13 @@ class ANT:
                 console.print('[bold red]ANT only ALLOWS Movies.')
             meta['skipping'] = "ANT"
             return []
+
+        if meta.get('bloated', False):
+            if not meta['unattended']:
+                console.print('[bold red]ANT does not allow bloated releases.')
+            meta['skipping'] = "ANT"
+            return []
+
         dupes = []
         params = {
             'apikey': self.config['TRACKERS'][self.tracker]['api_key'].strip(),
