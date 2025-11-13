a325fb3f6f456613819f6a560eb1a00b74936404
Audionut <audionut11@gmail.com>
2025-11-13 23:23:15 +1000

    RTV: refactor tv year handling

--- src/trackers/RTF.py
+++ src/trackers/RTF.py
@@ -120,8 +120,8 @@ class RTF():
             return []
 
         year = meta.get('year')
-        if meta.get('category') == "TV" and meta.get('tv_year') is not None:
-            year = meta['tv_year']
+        if meta.get('category') == "TV" and meta.get('tvdb_episode_year') is not None:
+            year = int(meta['tvdb_episode_year'])
         if datetime.date.today().year - year <= 9:
             console.print("[red]Content must be older than 10 Years to upload at RTF")
             meta['skipping'] = "RTF"
