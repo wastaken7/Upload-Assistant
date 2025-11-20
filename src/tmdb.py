3915ed991ee89c2f30bb48e4f742ecc7047159e4
Audionut <audionut11@gmail.com>
2025-11-20 20:21:02 +1000

    always attempt mal

--- src/tmdb.py
+++ src/tmdb.py
@@ -1000,15 +1000,14 @@ async def tmdb_other_meta(
         original_language = original_language_from_tmdb
 
     # Get anime information if applicable
-    if anime:
-        if category == "MOVIE":
-            filename = filename
-        else:
-            filename = path
-        mal_id, retrieved_aka, anime, demographic = await get_anime(
-            media_data,
-            {'title': title, 'aka': retrieved_aka, 'mal_id': 0, 'filename': filename}
-        )
+    if category == "MOVIE":
+        filename = filename
+    else:
+        filename = path
+    mal_id, retrieved_aka, anime, demographic = await get_anime(
+        media_data,
+        {'title': title, 'aka': retrieved_aka, 'mal_id': 0, 'filename': filename}
+    )
 
     if mal_manual is not None and mal_manual != 0:
         mal_id = mal_manual
