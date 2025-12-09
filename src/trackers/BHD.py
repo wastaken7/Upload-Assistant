5c69c2bdcabbd413e86903c650cf30b0bf932db0
Audionut <audionut@riseup.net>
2025-12-09 18:17:36 +1000

    BHD: adjust size bbcode for new site fix

--- src/trackers/BHD.py
+++ src/trackers/BHD.py
@@ -300,7 +300,7 @@ class BHD():
                     else:
                         await desc.write(f"[url={web_url}][img width=350]{img_url}[/img][/url] ")
                 await desc.write("[/align]")
-            await desc.write(f"\n[align=right][url=https://github.com/Audionut/Upload-Assistant][size=4]{meta['ua_signature']}[/size][/url][/align]")
+            await desc.write(f"\n[align=right][url=https://github.com/Audionut/Upload-Assistant][size=10]{meta['ua_signature']}[/size][/url][/align]")
             await desc.close()
         return
 
