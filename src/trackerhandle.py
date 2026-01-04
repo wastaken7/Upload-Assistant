7cc15d73549101f7217a924d1c4d0094731c1ea5
NeoByte <thatneobyte@gmail.com>
2026-01-04 11:45:43 +0100

    Fix manual description handling (#1099)

--- src/trackerhandle.py
+++ src/trackerhandle.py
@@ -145,7 +145,7 @@ async def process_trackers(meta, config, client, console, api_trackers, tracker_
                         manual_tracker = manual_tracker.replace(" ", "").upper().strip()
                         tracker_class = tracker_class_map[manual_tracker](config=config)
                         if manual_tracker in api_trackers:
-                            await DescriptionBuilder(config).unit3d_edit_desc(meta, tracker_class.tracker, tracker_class.signature)
+                            await DescriptionBuilder(config).unit3d_edit_desc(meta, "MANUAL")
                         else:
                             await tracker_class.edit_desc(meta)
                 url = await package(meta)
