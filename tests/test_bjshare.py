46100d94c1917450915e4cf485df092c86d54517
wastaken7 <46319635+wastaken7@users.noreply.github.com>
2026-08-01 15:57:45 -0300

    feat(bjshare): use the database overview and use unidecode in `get_tags()` (#171)

--- tests/test_bjshare.py
+++ tests/test_bjshare.py
@@ -81,3 +81,28 @@ def test_search_existing_queries_title_once_without_media_identifiers():
     asyncio.run(tracker.search_existing(meta))
 
     assert tracker.session.calls == [{"searchstr": "Example"}]  # noqa: S101
+
+
+def test_get_database_overview_extracts_synopsis():
+    html = """
+    <div class="box torrent_description">
+        <div class="body">
+            <blockquote>Em busca de uma vida melhor, Lu Xiao Fan deixa o interior...</blockquote>
+            <blockquote class="center"><iframe class="youtube" src="http://example.com"></iframe></blockquote>
+        </div>
+    </div>
+    """
+    soup = BeautifulSoup(html, "html.parser")
+    tracker = object.__new__(BJShare)
+    overview = tracker.get_database_overview(soup)
+    assert overview == "Em busca de uma vida melhor, Lu Xiao Fan deixa o interior..."  # noqa: S101
+
+
+def test_get_overview_returns_database_overview_when_already_has_the_info():
+    tracker = object.__new__(BJShare)
+    tracker.main_tmdb_data = {}
+    BJShare.already_has_the_info = True
+    BJShare.database_overview = "Sinopse do site BJ-Share"
+
+    result = asyncio.run(tracker.get_overview())
+    assert result == "Sinopse do site BJ-Share"  # noqa: S101
