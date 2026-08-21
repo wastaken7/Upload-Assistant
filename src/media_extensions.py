f7f0ee50cd6f0851e2facff05082d3404fc27632
wastaken7 <46319635+wastaken7@users.noreply.github.com>
2026-08-21 09:51:45 -0300

    refactor: centralize music media extensions

--- src/media_extensions.py
+++ src/media_extensions.py
@@ -3,3 +3,5 @@
 from __future__ import annotations
 
 VIDEO_EXTENSIONS = frozenset({".avi", ".m2ts", ".m4v", ".mkv", ".mov", ".mp4", ".mpeg", ".mpg", ".ts", ".webm", ".wmv"})
+AUDIO_EXTENSIONS = frozenset({".flac", ".mp3", ".m4a", ".aac", ".ac3", ".dts", ".wav", ".aiff", ".alac", ".ogg", ".opus", ".ape", ".wv"})
+ARTWORK_EXTENSIONS = frozenset({".jpg", ".jpeg", ".png", ".webp"})
