43daead6941df6822879694f4e80e7bc0c6bbbe0
wastaken7 <46319635+wastaken7@users.noreply.github.com>
2026-09-10 19:13:41 +0000

    chore(deps): update 7zip to 26.03

--- bin/binary_dependency_pins/seven_zip.py
+++ bin/binary_dependency_pins/seven_zip.py
@@ -1,11 +1,11 @@
-"""Release and integrity pins for 7-Zip."""
+"""Release and integrity pins for 7zip."""
 
-VERSION = "26.01"
+VERSION = "26.03"
 REPOSITORY = "ip7z/7zip"
 SHA256_BY_ASSET = {
-    "7z2601-linux-arm.tar.xz": "72c19911abb6964fcf85ebe213dfcee57bad892345e03bb940c5a27a1050b3bf",
-    "7z2601-linux-arm64.tar.xz": "39f8c9070c300a63c7484d9a983119ef3edf841e1ddf69f1affae29fdec5f612",
-    "7z2601-linux-x64.tar.xz": "8ea0fc8a135e7b848e80a4116fe22dff56c8c4518dde1f43cce67f4e340b437a",
-    "7z2601-mac.tar.xz": "0b6b930dbf82742e3f1014c35072a6b8b3aab183fece348e7f723675f1c5bea2",
-    "7zr.exe": "abcf64ae1cbafddb5395e4cdd3bdc7e3e0561d54a0c6380e3dd43bdbffe519a2",
+    "7z2603-linux-arm.tar.xz": "4efd099f6112b39054faea9424ded044b7bcaaf494dba49b918daa781297f57f",
+    "7z2603-linux-arm64.tar.xz": "2389ba20e4d8295e8709c20b6263b69bd1ec4972fe38a04ad7a1badbf595b996",
+    "7z2603-linux-x64.tar.xz": "dc99eff5008f1ab79bd7084c68513701547a808a89502bf4133683535ab3c695",
+    "7z2603-mac.tar.xz": "5ca87677072c59f5602e5c49baa27d4694bacd2259b4e507f0094249d4281480",
+    "7zr.exe": "ad4c82fadcbdf93c03b4fc440f300509c7d60c5c2f4d183e35d9d70d6957037d",
 }
