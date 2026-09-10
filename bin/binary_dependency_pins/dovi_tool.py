013add3ca25ad4af3b00f8c4e34b8ba7b79c84cb
wastaken7 <46319635+wastaken7@users.noreply.github.com>
2026-09-10 19:13:42 +0000

    chore(deps): update dovi_tool to 2.3.4

--- bin/binary_dependency_pins/dovi_tool.py
+++ bin/binary_dependency_pins/dovi_tool.py
@@ -1,11 +1,11 @@
 """Release and integrity pins for dovi_tool."""
 
-VERSION = "2.3.3"
+VERSION = "2.3.4"
 REPOSITORY = "quietvoid/dovi_tool"
 SHA256_BY_ASSET = {
-    "dovi_tool-2.3.3-aarch64-pc-windows-msvc.zip": "559ed634ef0b956ab89ed965b4920371bb228983f105d99fafeb372a8190c872",
-    "dovi_tool-2.3.3-aarch64-unknown-linux-musl.tar.gz": "daf538c275f4e702219ce8eb61db28382193ac9d0126e1ef4185a88303af4485",
-    "dovi_tool-2.3.3-universal-macOS.zip": "b113c83fed2d8d7ed9e43f0428d02fa0d0030e20965fc24a3cd4d48597d88685",
-    "dovi_tool-2.3.3-x86_64-pc-windows-msvc.zip": "37ae198f2a535c910befad39fc09c21cded76bf3ef2d5459d542e58c2c158311",
-    "dovi_tool-2.3.3-x86_64-unknown-linux-musl.tar.gz": "5dae82cb2becd3b9fd726127f936a8d32635e60746d16238fdfded12aa05988c",
+    "dovi_tool-2.3.4-aarch64-pc-windows-msvc.zip": "4056206e28f7d733fd8a29add91d286d22956b41138d6ae0d51dbbf2289d9483",
+    "dovi_tool-2.3.4-aarch64-unknown-linux-musl.tar.gz": "b4f22a7db56954efe4ed8d02276d0f991799e84602f3584711be6c9df950cb15",
+    "dovi_tool-2.3.4-universal-macOS.zip": "30d4f512eb67b7f1632cd28be5c62989bedad23f4710578aafc20c384b48ece6",
+    "dovi_tool-2.3.4-x86_64-pc-windows-msvc.zip": "0d9733d311dfe49f9dca0c350f7e6d978182673d333c6f07f7365daf9e6c81eb",
+    "dovi_tool-2.3.4-x86_64-unknown-linux-musl.tar.gz": "1844258e13c26607b32224bf1fa82b595d3b35949f5467405fda560daad32b3f",
 }
