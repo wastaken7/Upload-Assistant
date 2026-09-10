4ba75a534ac0478d42b864ddd7ba1ca9471eb834
wastaken7 <46319635+wastaken7@users.noreply.github.com>
2026-09-10 19:13:43 +0000

    chore(deps): update par2 to v1.5.0

--- bin/binary_dependency_pins/par2.py
+++ bin/binary_dependency_pins/par2.py
@@ -1,12 +1,12 @@
-"""Release and integrity pins for par2cmdline-turbo."""
+"""Release and integrity pins for par2."""
 
-VERSION = "v1.4.0"
+VERSION = "v1.5.0"
 REPOSITORY = "animetosho/par2cmdline-turbo"
 SHA256_BY_ASSET = {
-    "par2cmdline-turbo-1.4.0-linux-amd64.zip": "0be495172b4b8aeabda39c493e47de652813fab88ae745c8633e901c05494281",
-    "par2cmdline-turbo-1.4.0-linux-arm64.zip": "1bb2acb2c549bb3a2e91be3ac6291b00d4b657a56ab23f763f2161ffe7df0fcd",
-    "par2cmdline-turbo-1.4.0-macos-amd64.zip": "29ebb3629911a5b3ce4cdd8723a551a2877771b633630f443d99637559ef76be",
-    "par2cmdline-turbo-1.4.0-macos-arm64.zip": "926139d3cf18f6c4e4aeb25d6fc12b758cdf4936788fb46acd18caf21ffa9a15",
-    "par2cmdline-turbo-1.4.0-win-arm64.zip": "89870943c142b1360ab79bb287e47d5bffc686e159581750917207bbad2f6dcb",
-    "par2cmdline-turbo-1.4.0-win-x64.zip": "7905d1d6aced2b2ca30d824b4954e6bf740dc9d6cfec718b0ab146b5fc0d6327",
+    "par2cmdline-turbo-1.5.0-linux-amd64.zip": "5a9f64386813456693c2ea1fb7649436fe7544bbdf97fd73b3483dfcc8aca464",
+    "par2cmdline-turbo-1.5.0-linux-arm64.zip": "3afb2f0b319fc4e6353c6d4261994757cbe3189f968a72420b4ba1d63f9c9a70",
+    "par2cmdline-turbo-1.5.0-macos-amd64.zip": "3786d7c947ecb8048d688e0fdca3a4ce7e945d2bd248f54523ecc8d086e143cd",
+    "par2cmdline-turbo-1.5.0-macos-arm64.zip": "d016436356ef706c50ad5a4ee6316eef8138ab8d8177732ab97b9c2ad95b0990",
+    "par2cmdline-turbo-1.5.0-win-arm64.zip": "bb73312747b706dcedd13b8d4de7acd20e17367ebb0b26ebc3ea7d55ea744735",
+    "par2cmdline-turbo-1.5.0-win-x64.zip": "873a6f25822415f432224cc6f734407ca2732004184e002960fc68ed1e99c5d7",
 }
