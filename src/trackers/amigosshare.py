6746b1155f707805cbf58b7e1e50862c5c99a866
wastaken7 <46319635+wastaken7@users.noreply.github.com>
2026-09-23 14:28:13 -0300

    fix: update moderation queue comments and logic (#598)

--- src/trackers/amigosshare.py
+++ src/trackers/amigosshare.py
@@ -1275,8 +1275,10 @@ class AmigosShare:
         if not self.config["TRACKERS"][self.tracker].get("uploader_status", False):
             return False
 
+        config_flag = self.config["TRACKERS"][self.tracker].get("modq")
         if meta.modq:
-            logger.info(f"{self.tracker}: Sending to the moderation queue.")
+            return False
+        if config_flag is True:
             return False
 
         return True
